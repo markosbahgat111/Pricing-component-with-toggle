50931116c937425db7ae854da4c933eeb39b2b2a
markosbahgat111 <markos.bahgat111@gmail.com>
2022-03-29 06:07:55 +0200

    Organize Code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React, { useState } from 'react';
-import data from './data.json';
-import './App.css';
-import PricingComponent from './components/pricingCard/pricingCard.components';
-import Switch from '@mui/material/Switch';
-
-function App()
-{
-  const [plan, setPlan] = useState(false)
-  console.log(data)
-  return (
-    <main className='flex flex-col justify-center items-center overflow-auto h-screen m-auto w-screen'>
-      <div>
-        <h1>OUR PRICING</h1>
-        <section>
-          <span></span>
-          <Switch  />
-          <span></span>
-        </section>
-      </div>
-      <div className='flex flex-row h-3/4 flex-wrap md:flex-wrap lg:flex-nowrap sm:w-3/4 md:w-1/2 lg:w-11/12 justify-center items-center'>
-          {data.Cards.map(card => (
-            <PricingComponent data={card} key={card.id} plan={plan}/>
-          ))}
-      </div>
-    </main>
-  );
+import React, { useState } from "react";
+import data from "./data.json";
+import PricingComponent from "./components/pricingCard/pricingCard.components";
+import IOSSwitch from "./components/iosSwitch/Switch.component";
+function App() {
+	const [plan, setPlan] = useState(false);
+	console.log(data);
+	return (
+		<main className="flex flex-col justify-center items-center overflow-auto h-screen m-auto w-screen text-center">
+			<div className="mb-9">
+				<h1 className="text-2xl font-bold text-gray-500 mb-5">OUR PRICING</h1>
+				<section>
+					<span className="text-gray-400">Annually</span>
+					<IOSSwitch sx={{ mx: 3 }} defaultChecked onClick={() => setPlan((plan) => !plan)} />
+					<span className="text-gray-400">Monthly</span>
+				</section>
+			</div>
+			<div className="flex flex-row h-2/3 w-11/12 flex-wrap  md:flex-wrap lg:flex-nowrap sm:w-3/4 md:w-2/3 justify-center items-center">
+				{data.Cards.map((card) => (
+					<PricingComponent data={card} key={card.id} plan={plan} />
+				))}
+			</div>
+		</main>
+	);
 }
 
 export default App;
--- src/components/pricingCard/pricingCard.components.jsx
+++ src/components/pricingCard/pricingCard.components.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
 
-const PricingComponent = ({ data, plan }) =>
-{
-    const { annually, monthly } = data;
-    console.log(data, plan)
-    return ( 
-        <main className='bg-white w-full h-5/6  flex flex-col justify-start p-9 m-5 items-center text-center'>
-            <section>
-                <header className='uppercase'>{data.cardName}</header>
-                <h1 className='text-6xl'>${(plan ? monthly : annually).Price}</h1>
-            </section>
-            <section >
-                <hr />
-                <span className='capitalize'>{(plan ? monthly : annually).storage}</span>
-                <hr />
-                <span className='capitalize'>{(plan ? monthly : annually).sendCapacity}</span>
-                <hr />
-                <span className='capitalize'>{(plan ? monthly : annually).usersAllowed} Users Allowed</span>
-                <hr />
-            </section>
-            <button>
-                LEARN MORE
-            </button>
-        </main>
-     );
-}
- 
+const PricingComponent = ({ data, plan }) => {
+	const { annually, monthly } = data;
+	console.log(data, plan);
+	return (
+		<main
+			className="bg-white w-full rounded-xl flex flex-col justify-evenly p-5 items-center text-center px-9 my-9 lg:my-0  md:my-9"
+			id={data.cardName === "professional" && "active"}>
+			<section className="mb-9">
+				<header className="capitalize text-gray-500  mb-7 font-bold">{data.cardName}</header>
+				<h1 className="text-6xl text-gray-700 font-bold">${(!plan ? monthly : annually).Price}</h1>
+			</section>
+			<section className="w-full h-1/4 flex flex-col justify-evenly mt-5">
+				<hr />
+				<span className="capitalize text-gray-500 py-3">{(!plan ? monthly : annually).storage}</span>
+				<hr />
+				<span className="capitalize text-gray-500 py-3">
+					{(!plan ? monthly : annually).usersAllowed} Users Allowed
+				</span>
+				<hr />
+				<span className="capitalize text-gray-500 py-3">
+					send up to {(!plan ? monthly : annually).sendCapacity} GB
+				</span>
+				<hr />
+			</section>
+			<button className="text-white w-full rounded h-10 hover:border-2 text-sm font-bold mt-9">LEARN MORE</button>
+		</main>
+	);
+};
+
 export default PricingComponent;
